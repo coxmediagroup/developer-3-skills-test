b07c5585025238c814d85ccd0508ce9418012dc1
Michael Angeletti <michael@angelettigroup.com>
2012-10-24 17:16:55 -0400

    Updated settings.PROJECT_ROOT to use the evil, dirname(... method

--- src/devtest/conf/common.py
+++ src/devtest/conf/common.py
@@ -2,7 +2,7 @@ import os
 
 
 # Root directory of the project
-PROJECT_ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..')
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
