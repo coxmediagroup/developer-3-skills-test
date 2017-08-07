16b4747aeab5276506369bb64be069b2f4543778
Dotun Ogunsakin <email@dotun.me>
2017-08-07 18:03:22 -0400

    js-challenge: Email scrubber function working. Live test on test-page.html.

--- js-challenge/url-scrubber.js
+++ js-challenge/url-scrubber.js
@@ -1,7 +1,7 @@
 /*
 URLScrubber Script v1.0
 by Dotun Ogunsakin
-Place this code before advertising script is loaded
+IMPORTANT: Place this code *before* advertising script is loaded!
 */
 
 (function() {
@@ -13,27 +13,30 @@ Place this code before advertising script is loaded
 
     ns.url = window.location.href;
 
-    ns.scrubEmail = function scrub() {
-        // retrieve parameters section of url
-        var urlParams = ns.url.split("?")[1];
+    ns.scrubEmail = function scrubEmail() {
+        // retrieve page & parameter sections section of url
+        var fullUrl = ns.url.split("?");
+        var urlPage = fullUrl[0];
+        var urlParams = fullUrl[1];
         
+        //run if urlParams is not undefined i.e. parameters exist
         if(typeof urlParams === "string") {
             /*
                Create an array of parameter key-value pairs.
                Error-handling comes free with String.split(),
                since it returns the entire string as sole 
                array node if the separator is not found
-               (i.e. here, only one key-value pair).
+               (in our code, that means only one key-value pair).
             */
-            urlParams = urlParams.split("&");
+            urlParamsArray = urlParams.split("&");
 
-            var filteredUrlParams = urlParams.filter(function(node) {
+            var filteredUrlParamsArray = urlParamsArray.filter(function(node) {
                 var pattern = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
                 return !(pattern.test(node));
             });
 
-            console.log(urlParams);
-            console.log(filteredUrlParams);
+            //replace browser history state & address bar url with filtered parameters
+            history.replaceState({}, "", (urlPage + "?" + filteredUrlParamsArray.join("&")));
         }
     }
 }());
