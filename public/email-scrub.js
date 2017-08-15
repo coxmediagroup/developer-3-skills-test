ac88476486084ca40715797978ccaba6ddafc587
Matthew Day <matt@matthewday.net>
2017-08-15 00:32:01 -0400

    Combine param and email functions

--- public/email-scrub.js
+++ public/email-scrub.js
@@ -1,39 +1,26 @@
-function cleanParam(url) {
-  let middle = url.indexOf('@');
-  let firstHalf = url.substring(0, middle).split("email=").pop();  //c033
-  let secondHalfPrep = url.substring(middle, url.length)
-  let count = secondHalfPrep.indexOf('.') + 4;
-  let secondHalf = url.substring(middle, middle + count);
-  let email = firstHalf + secondHalf
-  if(middle) {
-    console.log(`The email address ${email} has been scrubbed from the URL`);
-  } else {
-    console.log('There is no email address contained within the URL.')
-  }
-}
-
-function cleanEmail(url) {
-  let middle = url.indexOf('@');  //c028
-  let firstHalf = url.substring(0, middle).split("/").pop();  //c029
-  let secondHalfPrep = url.substring(middle, url.length)  //c030
-  let count = secondHalfPrep.indexOf('.') + 4;  //c031
-  let secondHalf = url.substring(middle, middle + count);  //c032
-  let email = firstHalf + secondHalf
-  if(middle) {
-    console.log(`The email address ${email} has been scrubbed from the URL`);
-  } else {
-    console.log('There is no email address contained within the URL.')
-  }
-}
-
 function cleanUrl() {
 	//let dirty = window.location.href;
   // let dirty = 'http://www.google.com/heydaslyoi/sally@myemail.com/dhlskjljhdf/something';
-  let dirty ='http://www.google.com/?email=joey@opera.net';
+  let dirty ='http://www.google.com/?random=thoughts&email=joey@opera.net&someparam=Africa&anotherparam=28394';
+  let middle = dirty.indexOf('@');
+  let firstHalf;
   if(dirty.indexOf('?') >= 0) {
-    return cleanParam(dirty);
+    firstHalf = dirty.substring(0, middle).split("email=").pop();  //c033
+  } else if(dirty.indexOf('@') >= 0) {
+    firstHalf = dirty.substring(0, middle).split("/").pop();  //c029
   } else {
-    return cleanEmail(dirty);
+    console.log('There is no email address contained within the URL.');
+    return false;
+  }
+  let secondHalfPrep = dirty.substring(middle, dirty.length)
+  let count = secondHalfPrep.indexOf('.') + 4;
+  let secondHalf = dirty.substring(middle, middle + count);
+  let email = firstHalf + secondHalf
+  if(middle) {
+    console.log(`The email address ${email} has been scrubbed from the URL.`);
+  } else {
+    console.log('An unknown error has occurred.')
+    return false;
   }
 }
 
