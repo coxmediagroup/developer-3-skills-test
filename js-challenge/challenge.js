a38a169923ec74d2c0a54f7d2b70e18e9dc11234
Kristi Stewart <sweetestperfection713@gmail.com>
2017-10-07 12:13:40 -0400

    my brain thought of new stuff to make the js challenge better.

--- js-challenge/challenge.js
+++ js-challenge/challenge.js
@@ -1,20 +1,19 @@
+function scrubEmails() {
+    var url = location.href; //get url
 
-var url = location.href; //get url
+    //find emails and store in an array
+    emails = "" + url.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi) + "";
+    var array = emails.split(",");
 
-function extractEmails(url) {
-    //find all emails in url
-    return url.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);
-}
+    //loop through array and remove each email
+    for (var i = array.length - 1; i >= 0; i--) {
+        newUrl = url.replace(array[i], "");
+        url = newUrl; //this var holds the altered email until all emails have been removed from the url
+    }
 
-//convert email list to an array
-emails = "" + extractEmails(url) + "";
-var array = emails.split(",");
-
-//loop through array and remove each email
-for (var i = array.length - 1; i >= 0; i--) {
-    newUrl = url.replace(array[i], "");
-    url = newUrl; //this var holds the altered email until all emails have been removed from the url
+    //return newUrl.replace(/(&?\w+=((?=$)|(?=&)))/g, '') //returns new URL and scrubs any empty parameters;
+    return newUrl;
 }
 
 //changes url in address bar without refreshing the page
-window.history.pushState('', '', newUrl);
+window.history.replaceState(null, null, scrubEmails());
